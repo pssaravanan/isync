b6b6d19b3e0c3439645e693c597b1b49eadf13fb
Nithya Natarajan <nnithya@thoughtworks.com>
2012-09-20 01:32:20 +0530

    Nithya|Added a spike project to retrive and add contacts

---
+++ Spikes/AB/AB/AddressBookAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AddressBookAppDelegate.h
+//  AB
+//
+//  Created by Nithya on 19/09/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddressBookAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spikes/AB/AB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AB
+//
+//  Created by Nithya on 19/09/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AddressBookAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AddressBookAppDelegate class]));
+    }
+}
